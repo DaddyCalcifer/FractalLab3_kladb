54175f855b1f35081de7c2410b653fe14b5ff8f5
Никита Черничкин <nikita.macarov@yandex.ru>
2023-03-22 19:29:11 +0300

    Добавьте файлы проекта.

---
+++ FractalLab3/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace FractalLab3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 403);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 35);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ FractalLab3/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalLab3
+{
+    public partial class Form1 : Form
+    {
+        int Iterations = 6;
+
+        readonly Pen pen = new Pen(Color.Black, 2);
+        readonly List<Mark> Marks = new List<Mark>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            Text = "Кладбище";
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void DrawFractal(Graphics g)
+        {
+            Marks.Clear();
+
+            var root = new Mark(250, 50, 300);
+
+            DrawMark(g, root, 1);
+
+            root.Draw(g);
+
+            foreach(Mark mark in Marks)
+            {
+                mark.Draw(g);
+            }
+        }
+
+        private void DrawMark(Graphics g, Mark node, int depth)
+        {
+            if (depth > Iterations)
+            {
+                return;
+            }
+            
+            var p_left_top = new Mark(node.X, node.Y,node.Size/3);
+            var p_right_top = new Mark(node.X+(node.Size/3)*2, node.Y, node.Size / 3);
+            var p_left_bot = new Mark(node.X, node.Y+(node.Size/3)*2, node.Size / 3);
+            var p_right_bot = new Mark(node.X + (node.Size / 3) * 2, node.Y + (node.Size / 3) * 2, node.Size / 3);
+            
+            Marks.Add(node);
+            Marks.Add(p_left_top);
+            Marks.Add(p_right_top);
+            Marks.Add(p_right_bot);
+            Marks.Add(p_left_bot);
+
+            DrawMark(g, p_left_top, depth + 1);
+            DrawMark(g, p_right_top, depth + 1);
+            DrawMark(g, p_left_bot, depth + 1);
+            DrawMark(g, p_right_bot, depth + 1);
+        }
+
+        private class Mark
+        {
+            public Mark(int x, int y, int size)
+            {
+                X = x;
+                Y = y;
+                Size = size;
+            }
+
+            public int X { get; }
+            public int Y { get; }
+            public int Size { get; }
+            public void Draw(Graphics graph)
+            {
+                SolidBrush brush = new SolidBrush(Color.Black);
+                graph.FillRectangle(new SolidBrush(Color.White), new Rectangle(X, Y, Size, Size));
+                graph.FillRectangle(brush,X+Size/3,Y,(Size/3)+1,Size+1);
+                graph.FillRectangle(brush, X, (Y+Size/3), Size+1, (Size/3)+1);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Iterations = (int)numericUpDown1.Value-1;
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            DrawFractal(g);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
